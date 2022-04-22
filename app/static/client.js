6bcbeeceb258b204c543fb9c6549e3e3f277be1c
frankshicar <93965977+frankshicar@users.noreply.github.com>
2022-04-22 18:39:11 +0800

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -1,5 +1,9 @@
 var el = x => document.getElementById(x);
 
+const arr = [ "太陽伊布", "冰伊布", "火伊布", "葉伊布", "仙子伊布"];
+
+
+
 function showPicker() {
   el("file-input").click();
 }
@@ -29,7 +33,7 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `Result = ${arr[response["result"]]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
